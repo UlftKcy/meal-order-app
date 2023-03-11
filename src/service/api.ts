10fce53148479256d07c50130d0de667ef34f3cc
Ülfet <ukacay87@gmail.com>
2023-03-11 11:28:52 +0300

    updated service

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,9 +1,13 @@
 import axios from "axios";
 import { FetchIngredientsResponse, FetchMealsResponse, Meals } from "../types/types";
 
+const restaurantApi = axios.create({
+    baseURL: 'https://apis.career.otsimo.xyz/api/restaurant'
+});
+
 export const fetchMeals = async () => {
     try {
-        const response = await axios.get<FetchMealsResponse>("/listMeals");
+        const response = await restaurantApi.get<FetchMealsResponse>("/listMeals");
         return response.data;
     } catch (error: any) {
         throw new Error("Something is wrong", { cause: error });
@@ -12,7 +16,7 @@ export const fetchMeals = async () => {
 
 export const fetchIngredients = async () => {
     try {
-        const response = await axios.get<FetchIngredientsResponse>("/listIngredients");
+        const response = await restaurantApi.get<FetchIngredientsResponse>("/listIngredients");
         return response.data;
     } catch (error: any) {
         throw new Error("Something is wrong", { cause: error });
@@ -21,7 +25,7 @@ export const fetchIngredients = async () => {
 
 export const fetchMeal = async (id: string) => {
     try {
-        const response = await axios.get<Meals>(`/get/${id}`)
+        const response = await restaurantApi.get<Meals>(`/get/${id}`)
         return response.data;
     } catch (error: any) {
         throw new Error("Something is wrong", { cause: error });
